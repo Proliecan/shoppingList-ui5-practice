35cbb4abb4be4af18bb134e2c9265a4726fc8713
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-09-25 01:40:36 +0200

    Editable and non-editable lists

--- shopping-list-module/webapp/controller/MainView.controller.js
+++ shopping-list-module/webapp/controller/MainView.controller.js
@@ -5,16 +5,20 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast"
 ],
-    function (Controller,JSONModel, MessageToast) {
+    function (Controller, JSONModel, MessageToast) {
         "use strict";
 
         return Controller.extend("sl.ns.shoppinglistmodule.controller.MainView", {
-            getActiveListIndex: function() {
+            getActiveListIndex: function () {
                 let tabContainer = this.getView().byId("tabContainer");
-                    let id = tabContainer.getSelectedItem()
-                    let index = id.substring(id.length - 1, id.length);
-            
-                    return index;
+                let id = tabContainer.getSelectedItem()
+                let index = id.substring(id.length - 1, id.length);
+
+                return index;
+            },
+            getInput: function () {
+                let input = this.getView().byId("input");
+                return input;
             },
             onInit: function () {
                 let oModel = new JSONModel();
@@ -22,7 +26,7 @@ sap.ui.define([
                 this.getView().setModel(oModel);
             },
             onAdd: function (oEvent) {
-                let input = this.getView().byId("input");
+                let input = this.getInput();
                 let value = input.getValue();
                 input.setValue("");
 
@@ -30,8 +34,27 @@ sap.ui.define([
                 let data = oModel.getData();
 
                 let index = this.getActiveListIndex();
-                
+
                 data.lists[index].items.push({ name: value, quantity: 1 });
+            },
+            onTabChanged: function (oEvent) {
+                // get name of new tab
+                let name = oEvent.getParameter("item").getName();
+
+                let oModel = this.getView().getModel();
+                let data = oModel.getData();
+                // find list with name in model.lists
+                let list = data.lists.find(list => list.name === name);
+
+                let input = this.getInput();
+                input.setValue("");
+                if (list.editable != null) {
+                    input.setEnabled(list.editable);
+                }
+                else
+                    input.setEnabled(true);
+
+
             }
         });
     });
