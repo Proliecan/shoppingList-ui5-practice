0c8c41f8d6a381da7d0a4f2049be358e7a9cf3a7
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-09-25 00:27:36 +0200

    Multiple list support

--- shopping-list-module/webapp/controller/MainView.controller.js
+++ shopping-list-module/webapp/controller/MainView.controller.js
@@ -1,3 +1,5 @@
+
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
@@ -6,24 +8,30 @@ sap.ui.define([
     function (Controller,JSONModel, MessageToast) {
         "use strict";
 
-        
         return Controller.extend("sl.ns.shoppinglistmodule.controller.MainView", {
+            getActiveListIndex: function() {
+                let tabContainer = this.getView().byId("tabContainer");
+                    let id = tabContainer.getSelectedItem()
+                    let index = id.substring(id.length - 1, id.length);
+            
+                    return index;
+            },
             onInit: function () {
                 let oModel = new JSONModel();
                 oModel.loadData("/model/list.json");
                 this.getView().setModel(oModel);
-                console.log(oModel);
             },
-
             onAdd: function (oEvent) {
                 let input = this.getView().byId("input");
                 let value = input.getValue();
                 input.setValue("");
-                MessageToast.show(value);
 
                 let oModel = this.getView().getModel();
                 let data = oModel.getData();
-                data.items.push({ name: value, quantity: 1 });
-            },
+
+                let index = this.getActiveListIndex();
+                
+                data.lists[index].items.push({ name: value, quantity: 1 });
+            }
         });
     });
