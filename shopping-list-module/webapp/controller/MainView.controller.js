0cf605a7ec12318af0ab4a47149e001998eacec9
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-09-24 17:00:28 +0200

    Simple list interface

--- shopping-list-module/webapp/controller/MainView.controller.js
+++ shopping-list-module/webapp/controller/MainView.controller.js
@@ -1,15 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
+    function (Controller,JSONModel, MessageToast) {
         "use strict";
 
+        
         return Controller.extend("sl.ns.shoppinglistmodule.controller.MainView", {
             onInit: function () {
+                let oModel = new JSONModel();
+                oModel.loadData("/model/list.json");
+                this.getView().setModel(oModel);
+                console.log(oModel);
+            },
 
-            }
+            onAdd: function (oEvent) {
+                let input = this.getView().byId("input");
+                let value = input.getValue();
+                input.setValue("");
+                MessageToast.show(value);
+
+                let oModel = this.getView().getModel();
+                let data = oModel.getData();
+                data.items.push({ name: value, quantity: 1 });
+            },
         });
     });
